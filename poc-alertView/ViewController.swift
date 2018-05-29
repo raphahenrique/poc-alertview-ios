68ecb884d7512b40e9066401877cc5a56ad2dbb2
Raphael Henrique <raphaelsilva@natura.net>
2018-05-29 15:15:05 -0300

    Initial Commit

---
+++ poc-alertView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  poc-alertView
+//
+//  Created by Raphael Natura on 29/05/18.
+//  Copyright © 2018 Raphael Natura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ poc-alertViewTests/poc_alertViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  poc_alertViewTests.swift
+//  poc-alertViewTests
+//
+//  Created by Raphael Natura on 29/05/18.
+//  Copyright © 2018 Raphael Natura. All rights reserved.
+//
+
+import XCTest
+@testable import poc_alertView
+
+class poc_alertViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
